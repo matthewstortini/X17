206b3d17a6e62cb98f31c7601ab509c2369b894d
matthewstortini <stortm2@uw.edu>
2020-03-02 12:54:09 -0700

    adding in second foil to simulations

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -134,35 +134,66 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
    bege_logical->SetVisAttributes(begeVisAtt);
 
    //
-   // Lithium target foil
+   // first lithium target foil
    //
-   G4Tubs* foil_solid
-      = new G4Tubs("foil",   // its name
-            0*cm,            // innerRadius
-            3.5*cm,          // outerRadius
-            1.0*mm,          // half-height 
-            0,               // start angle
-            2*M_PI);         // spanning angle
+   G4Tubs* foil_1_solid
+      = new G4Tubs("foil_1",   // its name
+            0*cm,              // innerRadius
+            3.5*cm,            // outerRadius
+            0.0150*mm,         // half-height 
+            0,                 // start angle
+            2*M_PI);           // spanning angle
 
-   G4LogicalVolume* foil_logical
+   G4LogicalVolume* foil_1_logical   
       = new G4LogicalVolume(
-            foil_solid,            // its solid
-            lithium,               // its material
-            "foil");               // its name
+            foil_1_solid,            // its solid
+            lithium,                 // its material
+            "foil_1");               // its name
 
-   G4VPhysicalVolume* foil_phys
+   G4VPhysicalVolume* foil_1_phys
+      = new G4PVPlacement(
+            0,                           // no rotation
+            G4ThreeVector(0,0,-10*mm),   // at (0,0,-10) mm
+            foil_logical,                // its logical volume                         
+            "foil_1",                    // its name
+            logicWorld,                  // its mother volume
+            false,                       // no boolean operation
+            0,                           // copy number
+            checkOverlaps);              // checking overlaps 
+
+   G4VisAttributes* foilVisAtt = new G4VisAttributes(G4Colour(0,1.0,0));
+   foil_1_logical->SetVisAttributes(foilVisAtt);
+
+   //
+   // second lithium target foil
+   //
+   G4Tubs* foil_2_solid
+      = new G4Tubs("foil_2",   // its name
+            0*cm,              // innerRadius
+            3.5*cm,            // outerRadius
+            0.0075*mm,         // half-height 
+            0,                 // start angle
+            2*M_PI);           // spanning angle
+
+   G4LogicalVolume* foil_2_logical   
+      = new G4LogicalVolume(
+            foil_2_solid,            // its solid
+            lithium,                 // its material
+            "foil_2");               // its name
+
+   G4VPhysicalVolume* foil_2_phys   
       = new G4PVPlacement(
             0,                      // no rotation
             G4ThreeVector(0,0,0),   // at (0,0,0) mm
             foil_logical,           // its logical volume                         
-            "foil",                 // its name
-            logicWorld,             // its mother volume
-            false,                  // no boolean operation
-            0,                      // copy number
-            checkOverlaps);         // checking overlaps 
+            "foil_2",               // its name
+            logicWorld,                  // its mother volume
+            false,                       // no boolean operation
+            0,                           // copy number
+            checkOverlaps);              // checking overlaps 
 
    G4VisAttributes* foilVisAtt = new G4VisAttributes(G4Colour(0,1.0,0));
-   foil_logical->SetVisAttributes(foilVisAtt);
+   foil_2_logical->SetVisAttributes(foilVisAtt);
 
 return physWorld;
 
