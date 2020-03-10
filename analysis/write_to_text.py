714a44021d5dfcd307d6ac85894573fa62722e9c
matthewstortini <stortm2@uw.edu>
2020-03-10 10:09:54 -0600

    writing .txt files to correct directory

--- analysis/write_to_text.py
+++ analysis/write_to_text.py
@@ -10,6 +10,7 @@ if(len(sys.argv) != 2):
 with open("data.json") as f:
     data = json.load(f)
 data_dir = os.path.expandvars(data["data_dir"])
+x17_dir = os.path.expandvars(data["x17_dir"])
 
 # read in pandas dataframe
 df =  pd.read_hdf("{}/{}".format(data_dir,sys.argv[1]))
@@ -18,17 +19,17 @@ df = df.reset_index(drop=True)
 
 print(df)
 
-x_positions_file = open("X17_xpositions.txt", "w")
+x_positions_file = open("{}/X17_xpositions.txt".format(x17_dir), "w")
 for i in range(len(df)):
     x_positions_file.write("{}\n".format(df['x'][i]))
 x_positions_file.close()
 
-y_positions_file = open("X17_ypositions.txt", "w")
+y_positions_file = open("{}/X17_ypositions.txt".format(x17_dir), "w")
 for i in range(len(df)):
     y_positions_file.write("{}\n".format(df['y'][i]))
 y_positions_file.close()
 
-z_positions_file = open("X17_zpositions.txt", "w")
+z_positions_file = open("{}/X17_zpositions.txt".format(x17_dir), "w")
 for i in range(len(df)):
     z_positions_file.write("{}\n".format(df['z'][i]))
 z_positions_file.close()
